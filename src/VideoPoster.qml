sfos-archive/sailfish-components-gallery

commit ff15187c9109964045cd0b253d061c6a6a8deef7

[sailfish-components-gallery] Add a duration property to VideoPoster. Contributes to JB#9168

src/VideoPoster.qml
@@ -10,14 +10,26 @@ MouseArea {
     property bool active
     property url source
     property string mimeType
+    property int duration
+    onDurationChanged: positionSlider.maximumValue = duration
 
     property bool playing: active && videoItem.player && videoItem.player.playbackState == MediaPlayer.PlayingState
+    readonly property bool _loaded: active
+                && videoItem.player
+                && videoItem.player.status >= MediaPlayer.Loaded
+                && videoItem.player.status < MediaPlayer.EndOfMedia
 
     Connections {
-        target: videoItem.active ? videoItem.player : null
+        target: videoItem._loaded ? videoItem.player : null
 
         onPositionChanged: positionSlider.value = videoItem.player.position / 1000
         onDurationChanged: positionSlider.maximumValue = videoItem.player.duration / 1000
+    }
+
+    onActiveChanged: {
+        if (!active) {
+            positionSlider.value = 0
+        }
     }
 
     // Poster
@@ -36,7 +48,10 @@ MouseArea {
 
         priority: Thumbnail.HighPriority
         fillMode: Thumbnail.PreserveAspectFit
-        visible: !videoItem.active || !videoItem.player || !videoItem.player.playbackState != MediaPlayer.StoppedState
+        opacity: !videoItem._loaded ? 1.0 : 0.0
+        Behavior on opacity { FadeAnimation { id: posterFade } }
+
+        visible: !videoItem._loaded || posterFade.running
     }
 
     Item {
@@ -44,9 +59,9 @@ MouseArea {
         height: videoItem.height
 
         opacity: videoItem.playing ? 0.0 : 1.0
-        Behavior on opacity { FadeAnimation {} }
+        Behavior on opacity { FadeAnimation { id: controlFade } }
 
-        visible: videoItem.player
+        visible: videoItem.player && (videoItem.playing || controlFade.running)
 
         Image {
             anchors.centerIn: parent
@@ -73,7 +88,13 @@ MouseArea {
             minimumValue: 0
             valueText: Format.formatDuration(value, Formatter.DurationShort)
 
-            onReleased: videoItem.player.seek(value * 1000)
+            onReleased: {
+                if (videoItem.active) {
+                    videoItem.player.source = videoItem.source
+                    videoItem.player.seek(value * 1000)
+                    videoItem.player.pause()
+                }
+            }
         }
     }
 }

rpm/sailfish-components-gallery.spec
@@ -1,7 +1,7 @@
 Name:       sailfish-components-gallery-qt5
 
 Summary:    Sailfish Gallery UI Components
-Version:    0.0.14
+Version:    0.0.15
 Release:    1
 Group:      System/Libraries
 License:    TBD

commit 0da31025f6ea8498fd82c7f06399b449dff45562

[sailfish-components-gallery] Fixes JB#9271: play button not visible when tapping a video
Regression from ff15187c9109964045cd0b253d061c6a6a8deef7

src/VideoPoster.qml
@@ -61,7 +61,7 @@ MouseArea {
         opacity: videoItem.playing ? 0.0 : 1.0
         Behavior on opacity { FadeAnimation { id: controlFade } }
 
-        visible: videoItem.player && (videoItem.playing || controlFade.running)
+        visible: videoItem.player && (!videoItem.playing || controlFade.running)
 
         Image {
             anchors.centerIn: parent